bb69019236712ed0a2d91fa0a3775d5477b676f4
menabil <nabil99cc@gmail.com>
2025-10-22 00:10:59 +0600

    inputAdd

--- js/script.js
+++ js/script.js
@@ -12,13 +12,23 @@ let dur = document.querySelector(".dur");
 let lig = document.querySelector(".lig");
 let black = document.querySelector("h1");
 let white = document.querySelector("h1");
+let hblack = document.querySelector("h3");
+let hwhite = document.querySelector("h3");
 
 dur.addEventListener("click", () => {
   document.body.style.background = "black";
   white.style.color = "white";
+  hwhite.style.color = "white";
 });
 
 lig.addEventListener("click", () => {
   document.body.style.background = "white";
   black.style.color = "black";
+  hblack.style.color = "black";
+});
+
+const seeOutput = document.querySelector("input");
+
+seeOutput.addEventListener("keyup", () => {
+  console.log(seeOutput.value);
 });
